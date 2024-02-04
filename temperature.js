77711f239b4770613a0a2a8a9c83e07bf2b5a075
arnabw <arnabsahawrk@gmail.com>
2024-02-04 19:47:04 +0600

    completed task1

---
+++ temperature.js
@@ -0,0 +1,9 @@
+//Write a function to convert temperature from Celsius to Fahrenheit.
+
+function temperature(celsius) {
+  return (9 / 5) * celsius + 32;
+}
+
+console.log(
+  `Temperature form in celsius 22 and in fahrenheit ${temperature(22)}.`
+);
